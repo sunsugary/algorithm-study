f5498e448578925a34eab2bb09393dcd5fc17f5d
chengfeng.zheng <chengfeng.zheng@lrlz.com>
2022-09-20 18:04:46 +0800

    Code03_UnRecursiveTraversalBT

--- src/basic/Class10/Code03_UnRecursiveTraversalBT.java
+++ src/basic/Class10/Code03_UnRecursiveTraversalBT.java
@@ -1,5 +1,7 @@
 package basic.Class10;
 
+import java.util.Stack;
+
 /**
  * @ClassName: Code03_UnRecursiveTraversalBT
  * @Author: 5753
@@ -7,6 +9,33 @@ package basic.Class10;
  * @Description: 非递归方式遍历实现二叉树的先中后遍历
  */
 public class Code03_UnRecursiveTraversalBT {
+    public static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    //先序遍历
+    public static void pre(Node head) {
+        if (head != null){
+            Stack<Node> stack = new Stack<>();
+            stack.push(head);
+            while (!stack.isEmpty()){
+                head = stack.pop();
+                System.out.println(head.value);
+                if (head.right != null){
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+    }
+    //
 
 
 }
